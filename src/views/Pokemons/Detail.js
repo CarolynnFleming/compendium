7fc87ea02eb989e8f2738f10dce907ea9d7dc5ae
CarolynnFleming <cfcatpink@gmail.com>
2022-04-26 16:41:44 -0700

    use effect

--- src/views/Pokemons/Detail.js
+++ src/views/Pokemons/Detail.js
@@ -1,9 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import PokemoCard from '../../components/Pokemon/PokemonCard';
+import PokemonCard from '../../components/Pokemon/PokemonCard';
 
 export default function PokemonDetail() {
     const[pokemon, setPokemon] = useState({});
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
+useEffect(() => {
+    const getPokemon = async () => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const { data } = await res.json();
+        const pokemonData = {
+            id: data.id,
+            name: `${data.name}`,
+        };
+        setPokemon(pokemonData);
+        setLoading(false);
+    };
+    getPokemon();
+}, [id]);
+
+return (
+    
+)
 }
