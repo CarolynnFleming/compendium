da18f1a764db275497a13ea8f43faf080c437d31
CarolynnFleming <cfcatpink@gmail.com>
2022-04-26 17:32:35 -0700

    handlesearch function set up

--- src/views/Pokemons/List.js
+++ src/views/Pokemons/List.js
@@ -3,12 +3,23 @@ import { Link } from 'react-router-dom';
 import PokemonCard from '../../components/Pokemon/PokemonCard';
 
 export default function PokemonList() {
-    const [ pokemon, setPokemon] = useState([]);
+    const [ pokemons, setPokemons] = useState([]);
     const [loading, setloading] = useState(true);
     const [search, setSearch] = useState('');
-    const [result, setResult] = useState([]);
+    const [results, setResults] = useState([]);
 
     const isSearching = !!search.length;
     const noResults = isSearching && !results.length;
     const pokemonList = isSearching ? results : pokemons;
+
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+        const filteredPokemons = pokemons.filter((pokemon) =>
+        pokemon.name
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase()
+        .trim())
+        );
+        setResults(filteredPokemons);
+    };
 }
